f43aa09424f485e73506650a893f14163361d0b5
Guilherme Dutra <guilherme.dutra.almeida@gmail.com>
2026-06-15 10:12:24 -0300

    Alterado mensagem ex002 e adicionado mais uma valor em ex 003

--- ex003.py
+++ ex003.py
@@ -1,4 +1,5 @@
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
-soma = n1+n2
-print('A soma entre {} e {} é igual a: {}'.format(n1,n2,soma))
+n3 = int(input('Digite mais um valor: '))
+soma = n1+n2+n3
+print('A soma entre {} e {} e {} é igual a: {}'.format(n1,n2,n3,soma))
